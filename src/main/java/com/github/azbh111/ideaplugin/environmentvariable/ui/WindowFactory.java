b2da95f9314fe0312f05ca90f875a60537038ba6
zhengyongpan <591145360@qq.com>
2024-02-25 17:27:05 +0800

    版本向下兼容到203

--- src/main/java/com/github/azbh111/ideaplugin/environmentvariable/ui/WindowFactory.java
+++ src/main/java/com/github/azbh111/ideaplugin/environmentvariable/ui/WindowFactory.java
@@ -1,6 +1,7 @@
 package com.github.azbh111.ideaplugin.environmentvariable.ui;
 
 import com.github.azbh111.ideaplugin.environmentvariable.ui.actions.*;
+import com.github.azbh111.ideaplugin.environmentvariable.utils.ApplicationUtils;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
@@ -11,10 +12,18 @@ import com.intellij.ui.content.ContentFactory;
 
 public class WindowFactory implements ToolWindowFactory, DumbAware {
     @Override
+    @SuppressWarnings("*")
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
         ToolWindowPanel toolWindowPanel = new ToolWindowPanel(project);
 
-        com.intellij.ui.content.Content content = ContentFactory.getInstance().createContent(toolWindowPanel.getComponent(), null, false);
+        // 兼容v<=203: ContentFactory.SERVICE.getInstance()
+        ContentFactory contentFactory;
+        if (ApplicationUtils.isVersionGtThan(233)) {
+            contentFactory = ContentFactory.getInstance();
+        } else {
+            contentFactory = ContentFactory.SERVICE.getInstance();
+        }
+        com.intellij.ui.content.Content content = contentFactory.createContent(toolWindowPanel.getComponent(), null, false);
         toolWindow.getContentManager().addContent(content);
 
         if (toolWindow instanceof ToolWindowEx) {
